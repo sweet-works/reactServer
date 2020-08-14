6757b04ef9d5d7b38c3f503bf70be939b68f230a
zawb-wangyaojie <wb-wangyaojie@zhongan.io>
2020-08-14 14:52:10 +0800

    配置省略文件后缀名

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,3 +1,10 @@
+/*
+ * @Autor: yaojie
+ * @Date: 2020-08-13 14:47:01
+ * @LastEditors: yaojie
+ * @LastEditTime: 2020-08-14 14:46:51
+ * @Description: pu
+ */
 const path = require('path')
 const config = {
     entry: {
@@ -6,6 +13,23 @@ const config = {
     output: {
         filename: '[name].[chunkHash:5].js',
         path: path.join(__dirname, '../build'),
+        publicPath: '/public', // 引入时会以这个开头
+    },
+    'resolve': {
+        'extensions': [
+            '.js',
+            '.jsx',
+            '.png',
+            '.gif',
+            '.jpg',
+            '.ico',
+            '.scss',
+            '.css'
+        ],
+        'alias': {
+            '@client': path.resolve(__dirname, '../client'),
+            '@server': path.resolve(__dirname, '../server')
+        }
     },
 }
 module.exports = config;
