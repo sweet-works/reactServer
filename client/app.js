c86c56d73c65c587653057cc3800ba2e113104ae
zawb-wangyaojie <wb-wangyaojie@zhongan.io>
2020-08-14 16:34:54 +0800

    最基本的 webpack 打包js 到html

--- client/app.js
+++ client/app.js
@@ -2,9 +2,10 @@
  * @Autor: yaojie
  * @Date: 2020-08-14 14:17:29
  * @LastEditors: yaojie
- * @LastEditTime: 2020-08-14 15:58:38
+ * @LastEditTime: 2020-08-14 16:29:46
  * @Description: '配置react 入口文件'
  */
+ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './commponents/App.jsx'
 ReactDOM.render(<App />, document.body)
--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -2,10 +2,11 @@
  * @Autor: yaojie
  * @Date: 2020-08-13 14:47:01
  * @LastEditors: yaojie
- * @LastEditTime: 2020-08-14 15:57:23
+ * @LastEditTime: 2020-08-14 16:28:10
  * @Description: pu
  */
-const path = require('path')
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin')
 const config = {
     mode: 'development',
     entry: {
@@ -13,8 +14,8 @@ const config = {
     },
     output: {
         filename: '[name].[chunkHash:5].js',
-        path: path.join(__dirname, '../dis'),
-        publicPath: '/public', // 引入时会以这个开头
+        path: path.join(__dirname, '../dist'),
+        publicPath: '', // 引入时会以这个开头
     },
     resolve: {
         extensions: [
@@ -40,6 +41,9 @@ const config = {
                 loader: 'babel-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new HTMLPlugin()
+    ]
 }
 module.exports = config;
